cd64dba86cee4acb0d65b94f4cc70bb4c771a286
Marko Valing <markovaling@solaireresort.com>
2025-04-02 14:19:18 +0000

    renaming

--- custom_components/agatark/coordinator.py
+++ custom_components/agatark/coordinator.py
@@ -1,4 +1,4 @@
-"""DataUpdateCoordinator for integration_blueprint."""
+"""DataUpdateCoordinator for agatark."""
 
 from __future__ import annotations
 
--- custom_components/agatark/switch.py
+++ custom_components/agatark/switch.py
@@ -1,14 +1,10 @@
-"""Binary sensor platform for integration_blueprint."""
+"""Switch platform for agatark."""
 
 from __future__ import annotations
 
-from typing import TYPE_CHECKING
+from typing import TYPE_CHECKING, Any
 
-from homeassistant.components.binary_sensor import (
-    BinarySensorDeviceClass,
-    BinarySensorEntity,
-    BinarySensorEntityDescription,
-)
+from homeassistant.components.switch import SwitchEntity, SwitchEntityDescription
 
 from .entity import IntegrationBlueprintEntity
 
@@ -20,10 +16,10 @@ if TYPE_CHECKING:
     from .data import IntegrationBlueprintConfigEntry
 
 ENTITY_DESCRIPTIONS = (
-    BinarySensorEntityDescription(
-        key="integration_blueprint",
-        name="Integration Blueprint Binary Sensor",
-        device_class=BinarySensorDeviceClass.CONNECTIVITY,
+    SwitchEntityDescription(
+        key="agatark",
+        name="Integration Switch",
+        icon="mdi:format-quote-close",
     ),
 )
 
@@ -33,9 +29,9 @@ async def async_setup_entry(
     entry: IntegrationBlueprintConfigEntry,
     async_add_entities: AddEntitiesCallback,
 ) -> None:
-    """Set up the binary_sensor platform."""
+    """Set up the switch platform."""
     async_add_entities(
-        IntegrationBlueprintBinarySensor(
+        IntegrationBlueprintSwitch(
             coordinator=entry.runtime_data.coordinator,
             entity_description=entity_description,
         )
@@ -43,19 +39,29 @@ async def async_setup_entry(
     )
 
 
-class IntegrationBlueprintBinarySensor(IntegrationBlueprintEntity, BinarySensorEntity):
-    """integration_blueprint binary_sensor class."""
+class IntegrationBlueprintSwitch(IntegrationBlueprintEntity, SwitchEntity):
+    """agatark switch class."""
 
     def __init__(
         self,
         coordinator: BlueprintDataUpdateCoordinator,
-        entity_description: BinarySensorEntityDescription,
+        entity_description: SwitchEntityDescription,
     ) -> None:
-        """Initialize the binary_sensor class."""
+        """Initialize the switch class."""
         super().__init__(coordinator)
         self.entity_description = entity_description
 
     @property
     def is_on(self) -> bool:
-        """Return true if the binary_sensor is on."""
+        """Return true if the switch is on."""
         return self.coordinator.data.get("title", "") == "foo"
+
+    async def async_turn_on(self, **_: Any) -> None:
+        """Turn on the switch."""
+        await self.coordinator.config_entry.runtime_data.client.async_set_title("bar")
+        await self.coordinator.async_request_refresh()
+
+    async def async_turn_off(self, **_: Any) -> None:
+        """Turn off the switch."""
+        await self.coordinator.config_entry.runtime_data.client.async_set_title("foo")
+        await self.coordinator.async_request_refresh()
